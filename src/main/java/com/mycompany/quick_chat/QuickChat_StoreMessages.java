7d6207fa5fa05f890d363b7dc4a3905c6d96f479
Student <Student@LabVM2047644.xj1bnntybocu1gmxzszikw10hc.jnbx.internal.cloudapp.net>
2026-06-14 11:52:23 +0200

    Created QuickChat_StoreMessages Java Class

---
+++ src/main/java/com/mycompany/quick_chat/QuickChat_StoreMessages.java
@@ -0,0 +1,190 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.quick_chat;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class QuickChat_StoreMessages {
+       
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int option;
+
+        do {
+
+            System.out.println("===== STORE MESSAGES MENU =====");
+            System.out.println("1. Display Stored Messages");
+            System.out.println("2. Display Longest Stored Message");
+            System.out.println("3. Search Message ID");
+            System.out.println("4. Search Recipient");
+            System.out.println("5. Delete Message");
+            System.out.println("6. Display Report");
+            System.out.println("7. Exit");
+
+            System.out.print("Choose option: ");
+            option = input.nextInt();
+            input.nextLine();
+
+            switch(option) {
+
+                case 1:
+                    displayStoredMessages();
+                    break;
+
+                case 2:
+                    displayLongestStoredMessage();
+                    break;
+
+                case 3:
+                    System.out.print("Enter Message ID: ");
+                    String id = input.nextLine();
+                    searchMessageID(id);
+                    break;
+
+                case 4:
+                    System.out.print("Enter Recipient Number: ");
+                    String recipient = input.nextLine();
+                    searchRecipient(recipient);
+                    break;
+
+                case 5:
+                    System.out.print("Enter Message Hash: ");
+                    String hash = input.nextLine();
+                    deleteMessage(hash);
+                    break;
+
+                case 6:
+                        displayReport();
+                    break;
+
+                case 7:
+                    System.out.println("Goodbye");
+                    break;
+
+                default:
+                    System.out.println("Invalid option.");
+            }
+
+        } while(option != 7);
+
+        input.close();
+    }
+
+    public static void displayStoredMessages() {
+
+        System.out.println("\nSTORED MESSAGES");
+
+        for(int i = 0; i < totalMessages; i++) {
+
+            if(status[i].equals("Stored")) {
+
+                System.out.println("-------------------------");
+                System.out.println("Sender: Developer");
+                System.out.println("Recipient: " + recipients[i]);
+                System.out.println("Message: " + messages[i]);
+            }
+        }
+    }
+
+    public static void displayLongestStoredMessage() {
+
+        String longest = "";
+
+        for(int i = 0; i < totalMessages; i++) {
+
+            if(status[i].equals("Stored")) {
+
+                if(messages[i].length() > longest.length()) {
+
+                    longest = messages[i];
+                }
+            }
+        }
+
+        System.out.println("\nLongest Stored Message:");
+        System.out.println(longest);
+    }
+
+    public static void searchMessageID(String id) {
+
+        boolean found = false;
+
+        for(int i = 0; i < totalMessages; i++) {
+
+            if(messageIDs[i].equals(id)) {
+
+                System.out.println("Recipient: " + recipients[i]);
+                System.out.println("Message: " + messages[i]);
+                found = true;
+            }
+        }
+
+        if(!found) {
+
+            System.out.println("Message ID not found.");
+        }
+    }
+
+    public static void searchRecipient(String recipient) {
+
+        boolean found = false;
+
+        for(int i = 0; i < totalMessages; i++) {
+
+            if(recipients[i].equals(recipient)) {
+
+                System.out.println(messages[i]);
+                found = true;
+            }
+        }
+
+        if(!found) {
+
+            System.out.println("No messages found.");
+        }
+    }
+
+    public static void deleteMessage(String hash) {
+
+        boolean found = false;
+
+        for(int i = 0; i < totalMessages; i++) {
+
+            if(messageHashes[i].equals(hash)) {
+
+                for(int j = i; j < totalMessages - 1; j++) {
+
+                    messageIDs[j] = messageIDs[j + 1];
+                    recipients[j] = recipients[j + 1];
+        
+    
+                }
+            }
+        }
+    }
+    
+   public static void displayReport() {
+
+        System.out.println("===== MESSAGE REPORT =====");
+
+        for(int i = 0; i < totalMessages; i++) {
+
+            System.out.println("-------------------------");
+            System.out.println("Message ID: " + messageIDs[i]);
+            System.out.println("Message Hash: " + messageHashes[i]);
+            System.out.println("Recipient: " + recipients[i]);
+            System.out.println("Message: " + messages[i]);
+            System.out.println("Status: " + status[i]);
+        }
+    }
+   
+}
